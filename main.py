2f680501d540f075174daad56fb4f2762a2eaf10
Lukas Valek <xvalek15@fit.vutbr.cz>
2018-11-09 23:25:42 +0100

    window size changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.PlaceholderGame import PlaceholderGame
 
 def main():
     game = PlaceholderGame()
-    game.init_screen('Cool game', (640, 480))
+    game.init_screen('Cool game', (1024, 512))
     game.run()
 
 
