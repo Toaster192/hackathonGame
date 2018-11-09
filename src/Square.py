1128bc8034066bafbdcbac109dba21d89bbe8d4b
0t4k4r <beinhauer.otakar@gmail.com>
2018-11-09 23:46:17 +0100

    removed pycache

---
+++ src/Square.py
@@ -0,0 +1,12 @@
+import pygame
+ 
+from GameObject import GameObject
+ 
+ 
+class Square(GameObject):
+    def __init__(self, x, y, w=32, h=32, color):
+        GameObject.__init__(self, x, y, w, h)
+        self.color = color
+ 
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.bounds)
