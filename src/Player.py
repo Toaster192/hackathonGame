8f7535dd52e07d0287c23c56b790cef91f2fe170
Lukas Valek <xvalek15@fit.vutbr.cz>
2018-11-09 23:10:58 +0100

    Player class improvments

--- src/Player.py
+++ src/Player.py
@@ -1,1 +1,10 @@
 class Player:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.jumping = False
+        self.left = False
+        self.right = False
