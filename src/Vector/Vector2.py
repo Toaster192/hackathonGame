ab3ed35e1d26065e943c3e072a785e14eb9636fb
Urumasi <Urumasi@email.cz>
2018-11-10 11:49:00 +0100

    Vector swizzling, few other functions

--- src/Vector/Vector2.py
+++ src/Vector/Vector2.py
@@ -1,4 +1,7 @@
 import numbers
+from re import match
+
+from . import Vector3
 
 
 class Vector2:
@@ -37,7 +40,28 @@ class Vector2:
             raise ArithmeticError
         return Vector2(self.x // other, self.y // other)
 
+    def length(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def distance(self, other):
+        if not isinstance(other, Vector2):
+            raise ArithmeticError
+        return (self - other).length()
+
     def manhattan_distance(self, other):
         if not isinstance(other, Vector2):
             raise ArithmeticError
         return abs(self.x - other.x) + abs(self.y - other.y)
+
+    # Swizzling
+    def __getattr__(self, key):
+        if match('^[xy]{2}$', key):
+            v = []
+            for c in key:
+                v.append(getattr(self, c))
+            return Vector2(*v)
+        elif match('^[xy]{3}$', key):
+            v = []
+            for c in key:
+                v.append(getattr(self, c))
+            return Vector3(*v)
