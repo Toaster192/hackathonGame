59c59980b2e0858d4ae470d19045651161adab6a
Urumasi <Urumasi@email.cz>
2018-11-10 03:56:42 +0100

    Added particle system

--- src/BlockGenerator.py
+++ src/BlockGenerator.py
@@ -18,35 +18,17 @@ class BlockGenerator():
         height = max(map((lambda t: t[1]),
                          generated_type)) + Config.BLOCK_WIDTH
 
-<<<<<<< HEAD
-		# Rotate - TODO
-		width = max(map((lambda t: t[0]), generated_type)) + 32
-		height = max(map((lambda t: t[1]), generated_type)) + 32
-=======
         y = - height
->>>>>>> 0e6bbc0cfb125b7712d095c63ff3065e9230d79d
 
         # AI
 
-<<<<<<< HEAD
-		# AI
-=======
         x = random.randint(0,
                            (Config.SCREEN_WIDTH - width)
                            // Config.BLOCK_WIDTH) * Config.BLOCK_WIDTH
->>>>>>> 0e6bbc0cfb125b7712d095c63ff3065e9230d79d
 
         color = random.choice(Colors.colors)
 
         block = Block(x, y, width, height, generated_type,
                       speed, "active", color)
 
-<<<<<<< HEAD
-		block = Block(x, y, width, height, generated_type, 
-			          speed, "falling", color)
-
-		return block
-
-=======
         return block
->>>>>>> 0e6bbc0cfb125b7712d095c63ff3065e9230d79d
--- src/PlaceholderGame.py
+++ src/PlaceholderGame.py
@@ -3,8 +3,9 @@ from src.GameField import GameField
 from src.Game import Game
 import src.Colors as Color
 import src.Config as Config
-# from src.TilePainter import paint_tile
+from src.TilePainter import paint_tile
 from src.Player import Player
+from .Particles import ParticleFieldEmitter
 
 
 class PlaceholderGame(Game):
@@ -14,8 +15,20 @@ class PlaceholderGame(Game):
         super().__init__()
         self.fps_font = None
         self.fps = 0
-        self.player1 = Player(50, 50, Config.PLAYER_WIDTH,
+        self.player1 = Player((Config.SCREEN_WIDTH) // 2,
+                              Config.GAMEFIELD_BOTTOM_BORDER -
+                              Config.PLAYER_HEIGHT,
+                              Config.PLAYER_WIDTH,
                               Config.PLAYER_HEIGHT, Color.RED)
+
+        self.emitter = ParticleFieldEmitter(color_begin=Color.GRAY,
+                                            color_end=Color.BLACK,
+                                            color_jitter=0.1, pos=(256, 256),
+                                            size=(32, 8), velocity=(16, -8),
+                                            velocity_jitter=(4, 4),
+                                            accel=(0, -4), gen_delay=0.01,
+                                            duration=4, size_begin=6,
+                                            size_end=0)
 
     # Gets called at the start of the game
     def init(self, window_name, size):
@@ -38,25 +51,26 @@ class PlaceholderGame(Game):
 
         if keys[pygame.K_LEFT]:
             print(self.player1.x, Config.GAMEFIELD_LEFT_BORDER)
-            if int(self.player1.x) <= int(Config.GAMEFIELD_LEFT_BORDER) + 1:
-                self.player1.v_x = 0
-                self.player1.x = Config.GAMEFIELD_LEFT_BORDER
-            else:
-                self.player1.moveLeft()
+            self.player1.moveLeft()
         if keys[pygame.K_RIGHT]:
-            print(Config.PLAYER_WIDTH)
-            if int(self.player1.x) >= ((int(Config.GAMEFIELD_RIGHT_BORDER) - 1) - Config.PLAYER_WIDTH):
-                self.player1.v_x = 0
-                self.player1.x = Config.GAMEFIELD_RIGHT_BORDER - Config.PLAYER_WIDTH
-            else:
-                self.player1.moveRight()
+            print(self.player1.x, Config.GAMEFIELD_RIGHT_BORDER)
+            self.player1.moveRight()
         if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
             self.player1.stopMoving()
         if keys[pygame.K_UP]:
             self.player1.jump()
 
+        if int(self.player1.x) < int(Config.GAMEFIELD_LEFT_BORDER):
+            self.player1.v_x = 0
+            self.player1.x = Config.GAMEFIELD_LEFT_BORDER
+        elif int(self.player1.x) > ((int(Config.GAMEFIELD_RIGHT_BORDER)) - Config.PLAYER_WIDTH):
+            self.player1.v_x = 0
+            self.player1.x = Config.GAMEFIELD_RIGHT_BORDER - Config.PLAYER_WIDTH
+
         self.player1.x += self.player1.v_x
         self.player1.y += self.player1.v_y
+
+        self.emitter.update(dt)
 
     # Called after loop(), renders the game screen
 
@@ -64,8 +78,8 @@ class PlaceholderGame(Game):
         self.surface.fill(Color.BLACK)
 
         pygame.draw.rect(self.surface, self.player1.color, pygame.Rect(
-                         self.player1.x, self.player1.y,
-                         self.player1.width, self.player1.height))
+            self.player1.x, self.player1.y,
+            self.player1.width, self.player1.height))
         self.game_field.draw(self.surface)
 
         # paint_tile(self.surface, 20, 20, 128, 128, Color.RED)
@@ -73,11 +87,13 @@ class PlaceholderGame(Game):
         # paint_tile(self.surface, 148, 20, 128, 128, Color.BLUE)
         # paint_tile(self.surface, 148, 148, 128, 128, Color.MAGENTA)
 
-        # paint_tile(self.surface, 60, 300, 16, 16, Color.ORANGE)
+        paint_tile(self.surface, 256, 256, 32, 32, Color.RED)
 
         pygame.draw.rect(self.surface, self.player1.color,
                          pygame.Rect(self.player1.x, self.player1.y,
                                      self.player1.width, self.player1.height))
+
+        self.emitter.render(self.surface)
 
         fps_surface = \
             self.fps_font.render('FPS: ' + str(self.fps), True, Color.GRAY)
@@ -90,7 +106,7 @@ class PlaceholderGame(Game):
         while self.running:
             for event in pygame.event.get():
                 self._handle_event(event)
-            self.loop(self.clock.get_time()/1000)
+            self.loop(self.clock.get_time() / 1000)
             self.render()
             self.clock.tick(60)
         self._clean_up()
